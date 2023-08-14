a010a8bd31a3e1e68e74ed5d6c528763c1a1c8fb
MAMURg <135414475+MAMURg@users.noreply.github.com>
2023-08-14 01:50:45 +0000

    Update View.java

--- src/main/View.java
+++ src/main/View.java
@@ -6,7 +6,7 @@ import Service.MedicationsServiceImpl;
 
 public class View {
     public static void main(String[] args) {
-        System.out.println("=== Medications ===");
+        System.out.println("=== Medications Warehouse ===");
         System.out.println(" Welcome to Medications Warehouse ");
         System.out.println("Version 2.0 created date-2023.06.10");
         System.out.println("Contact: \nName - Mamurjon Turdikulov \nemail - turdikulovmamur@gmail.com");
